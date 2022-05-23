4950d17caee61e1c2298ed71af5f24805a74c84d
burkay <bayirburkay@gmail.com>
2022-05-23 09:37:37 +0300

    some changes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const port = process.env.PORT || 8000;
 
@@ -14,11 +15,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Selam");
-});
-
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/coins", require("./routes/coinRoutes"));
 
+app.use(errorHandler);
 app.listen(port, () => console.log(`***SERVER STARTED ON PORT ${port}***`));
